c0fc8b78a634c115e5fdf6baf6e20a63410257c1
Maheilu <96106270+Maheilu@users.noreply.github.com>
2023-03-20 22:45:31 +0100

    lsp formatting für elixir

--- maheilu-functions.el
+++ maheilu-functions.el
@@ -3,7 +3,8 @@
   "versucht den Buffer abhängig vom major-mode zu formatieren"
   (interactive)
   (cond
-   ((eq major-mode #'c++-mode)
+   ((or (eq major-mode #'c++-mode)
+        (eq major-mode #'elixir-mode))
     (require 'lsp-mode)
     (lsp-format-buffer))
    (t (message "kein Formatter für %s gefunden" major-mode))))
