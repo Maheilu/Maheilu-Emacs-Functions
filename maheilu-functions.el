b6adf8346c289989002e8758fa3b7b12bfb5ba50
Maheilu <96106270+Maheilu@users.noreply.github.com>
2023-05-12 13:16:31 +0200

    R formatting

--- maheilu-functions.el
+++ maheilu-functions.el
@@ -1,6 +1,8 @@
 ;;; maheilu-functions.el --- Description -*- lexical-binding: t; -*-
+;;; commentary:
+;;; code:
 (defun maheilu-try-format ()
-  "versucht den Buffer abhängig vom major-mode zu formatieren"
+  "Versucht den Buffer abhängig vom major-mode zu formatieren."
   (interactive)
   (cond
    ((eq major-mode #'c++-mode)
@@ -9,6 +11,11 @@
    ((eq major-mode #'elixir-mode)
     (require 'elixir-format)
     (elixir-format))
+   ((eq major-mode #'ess-r-mode)
+    (require 'lsp-mode)
+    ;;; this needs the R language server and the R package styler
+    ;;; R packages "languageserver" and "styler" respectively
+    (lsp-format-buffer))
    (t (message "kein Formatter für %s gefunden" major-mode))))
 
 (provide 'maheilu-functions)
